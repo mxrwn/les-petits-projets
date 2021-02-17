5cc62b7db18e42a6d3ba8d3b3230f22d00c2c2ef
Ramzy Merad <Ramzy.merad@student.ehb.be>
2021-02-17 13:56:23 +0100

    Vanilla js fini

--- README.md
+++ README.md
@@ -1,29 +1,45 @@
 # Les petits projets
 
-- [ ] Modern digital clock
-- [ ] Random quotes
-- [ ] Counter project
-- [ ] Background image slider
-- [ ] Server express
-- [ ] To-Do App
-  - [ ] Vanilla JS
-  - [ ] React
-- [ ] Navigation menu
-- [ ] Battery charge animation
-- [ ] Basic calculator
-- [ ] Grocery list project
-- [ ] Drawing app
-- [ ] Login authentication
-- [ ] Interactive quiz app
-- [ ] Contact form
-- [ ] Video player
-- [ ] Music player
-- [ ] Mouseover element project
-- [ ] BMI calculator project
-- [ ] Tetris game
-- [ ] Number guessing game
-- [ ] Interactive app
-- [ ] Secret message Encoder\Decoder
-- [ ] Real time chat app
-- [ ] Front-end of an online store
-- [ ] Text-to-speech
+* [ ] Modern digital clock
+* [ ] Random quotes
+* [ ] Counter project
+* [ ] Background image slider
+* [ ] Server express
+* [ ] To-Do App
+  * [x] Vanilla JS
+  * [ ] React
+* [ ] Navigation menu
+* [ ] Battery charge animation
+* [ ] Basic calculator
+* [ ] Grocery list project
+* [ ] Drawing app
+* [ ] Login authentication
+* [ ] Interactive quiz app
+* [ ] Contact form
+* [ ] Video player
+* [ ] Music player
+* [ ] Mouseover element project
+* [ ] BMI calculator project
+* [ ] Tetris game
+* [ ] Number guessing game
+* [ ] Interactive app
+* [ ] Secret message Encoder\Decoder
+* [ ] Real time chat app
+* [ ] Front-end of an online store
+* [ ] Text-to-speech
+
+* [ ] # React
+  * [ ] create-react-app
+  * [ ] show message from state on page
+  * [ ] Create Todo Form
+  * [ ] Call function when the form is submitted
+  * [ ] Create a string state value to store the user input
+  * [ ] Listen for when the input changes and update state
+  * [ ] Log the user input when the form is submitted
+  * [ ] Create an array state value for todos
+  * [ ] Add a new todo into the todos array, with done: false
+  * [ ] Show the todos in a list
+  * [ ] Check done on a todo to mark it as done
+    * [ ] Show a line through the todo text
+  * [ ] Add a button to remove a todo
+  * [ ] Add a button to mark all todos as done
--- to-do-app/js/scripts/app.js
+++ to-do-app/js/scripts/app.js
@@ -1,3 +1,6 @@
+//Definir des variables
+let todos = [];
+let finishedtasks= [];
 // Recuperer les données de l'utilisateur
 window.onload = () => {
   // Récuperer l'element input avec id 'todo-input'
@@ -9,9 +12,13 @@ window.onload = () => {
       event.preventDefault()
       // Recuperer la valeur de l'input "todo-input"
       const newtodo = todo.value;
-      console.log(newtodo);
+      todo.value="";
+      todos.push({
+        id:todos.length,
+        text: newtodo
+      });
       // Le passer en tant qu'argument dans la function "AfficherTodo"
-      AfficherTodo(newtodo)
+      AfficherTodo();
     }
   })
 };
@@ -19,35 +26,39 @@ window.onload = () => {
 /**
  * @input {String} Rentrez l'input qui devrait etre affiché sur la page.
  */
+const AfficherTodo = () => {
+    let appendtodos = document.querySelector(".todos");
+    appendtodos.innerHTML="";
+todos.forEach(todo => {
+// Créer les differents elements et leurs donner le sattributs appropriés
+let todocard = document.createElement("div");
+todocard.classList.add("todo-card");
 
-let AfficherTodo = (input) => {
+let todohead = document.createElement("div");
+todohead.classList.add("todo-head");
 
-  // Créer les differents elements et leurs donner le sattributs appropriés
-  let todocard = document.createElement("div");
-  todocard.classList.add("todo-card");
+let todobody = document.createElement("div");
+todobody.classList.add("todo-body");
 
-  let todohead = document.createElement("div");
-  todohead.classList.add("todo-head");
+let todotext = document.createElement("h1");
+todotext.id = "todo-title";
+todotext.innerText = todo.text;
 
-  let todobody = document.createElement("div");
-  todobody.classList.add("todo-body");
+let tododone = document.createElement("div");
+tododone.classList.add("done");
+tododone.addEventListener("click",() => {Donetask(todo.id)})
 
-  let todotext = document.createElement("h1");
-  todotext.id = "todo-title";
-  todotext.innerText = input;
+let tododelete = document.createElement("div");
+tododelete.classList.add("delete");
+tododelete.id=todo.id;
+tododelete.addEventListener("click",() => {Deletetask(todo.id)})
 
-  let tododone = document.createElement("div");
-  tododone.classList.add("done");
+let tododoneimg = document.createElement("img");
+tododoneimg.src = "./images/check-mark.svg";
 
-  let tododelete = document.createElement("div");
-  tododelete.classList.add("delete");
-
-  let tododoneimg = document.createElement("img");
-  tododoneimg.src = "./images/check-mark.svg";
-
-  let tododeleteimg = document.createElement("img");
-  tododeleteimg.src = "./images/trash.svg";
-
+let tododeleteimg = document.createElement("img");
+tododeleteimg.src = "./images/trash.svg";
+    
   //append les different elements
   todohead.appendChild(todotext);
   tododone.appendChild(tododoneimg);
@@ -58,6 +69,84 @@ let AfficherTodo = (input) => {
   todocard.appendChild(todobody);
 
   //les rajouter sous la section ".todos"
-  let appendtodos = document.querySelector(".todos");
   appendtodos.appendChild(todocard);
+});
+  
+
+}
+
+/**
+ * @id {Number} id position pour delete l'element selectionner.
+ */
+
+//Tododelete button
+const Deletetask =(id)=>{
+    //check le type de variable
+    console.log(typeof id);
+    let foundtodo = todos.find(todo => todo.id == id);
+    let index= todos.indexOf(foundtodo);
+    console.log(foundtodo);
+    todos.splice(index,1);
+    console.log(todos);
+    AfficherTodo();
+}
+const Deletetaskdone =(id)=>{
+    //check le type de variable
+    console.log(typeof id);
+    let foundtodo = finishedtasks.find(todo => todo.id == id);
+    let index= finishedtasks.indexOf(foundtodo);
+    console.log(foundtodo);
+    finishedtasks.splice(index,1);
+    AfficherTodo();
+    AfficherFinished();
+}
+
+const Donetask=(id)=>{
+    console.log(typeof id);
+    let foundtodo = todos.find(todo => todo.id == id);
+    let index= todos.indexOf(foundtodo);
+    console.log(foundtodo);
+    todos.splice(index,1);
+    finishedtasks.push(foundtodo);
+    AfficherTodo();
+    AfficherFinished();
+}
+const AfficherFinished=()=>{
+    let appendtodos = document.querySelector(".todos-done");
+    appendtodos.innerHTML="";
+finishedtasks.forEach(todo => {
+// Créer les differents elements et leurs donner le sattributs appropriés
+let todocard = document.createElement("div");
+todocard.classList.add("todo-card");
+todocard.id="done";
+
+let todohead = document.createElement("div");
+todohead.classList.add("todo-head");
+
+let todobody = document.createElement("div");
+todobody.classList.add("todo-body");
+
+let todotext = document.createElement("h1");
+todotext.id = "todo-title";
+todotext.innerText = todo.text;
+
+let tododelete = document.createElement("div");
+tododelete.classList.add("delete");
+tododelete.id=todo.id;
+tododelete.addEventListener("click",() => {Deletetaskdone(todo.id)})
+
+
+let tododeleteimg = document.createElement("img");
+tododeleteimg.src = "./images/trash.svg";
+    
+  //append les different elements
+  todohead.appendChild(todotext);
+  tododelete.appendChild(tododeleteimg);
+  todobody.appendChild(tododelete);
+  todocard.appendChild(todohead);
+  todocard.appendChild(todobody);
+
+  //les rajouter sous la section ".todos"
+  appendtodos.appendChild(todocard);
+});
 }
