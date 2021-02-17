97850b60fd422cb72d8a4545e76d09e2057f5902
mxrwn <marwaneelkchiri@gmail.com>
2021-02-17 10:56:29 +0100

    Documentation

--- to-do-app/js/scripts/app.js
+++ to-do-app/js/scripts/app.js
@@ -1,56 +1,63 @@
 // Recuperer les données de l'utilisateur
 window.onload = () => {
-let todo= document.getElementById("todo-input");
-todo.addEventListener("keyup",(event)=>{
-    if(event.keyCode=== 13){
-        event.preventDefault();
-        console.log('enter')
-        let newtodo=todo.value;
-        console.log(newtodo);
-        AfficherTodo(newtodo);
-    }else{
-      console.log('keyup trigger')
+  // Récuperer l'element input avec id 'todo-input'
+  const todo = document.getElementById("todo-input");
+  // On event keyup
+  todo.addEventListener("keyup", (event) => {
+    // Verifier si la personne appuie sur enter
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      // Recuperer la valeur de l'input "todo-input"
+      const newtodo = todo.value;
+      console.log(newtodo);
+      // Le passer en tant qu'argument dans la function "AfficherTodo"
+      AfficherTodo(newtodo)
     }
-})
+  })
 };
 
+/**
+ * @input {String} Rentrez l'input qui devrait etre affiché sur la page.
+ */
 
+let AfficherTodo = (input) => {
 
-let AfficherTodo=(input)=>{
-let todocard= document.createElement("div");
-todocard.classList.add("todo-card");
+  // Créer les differents elements et leurs donner le sattributs appropriés
+  let todocard = document.createElement("div");
+  todocard.classList.add("todo-card");
 
-let todohead= document.createElement("div");
-todohead.classList.add("todo-head");
+  let todohead = document.createElement("div");
+  todohead.classList.add("todo-head");
 
-let todobody= document.createElement("div");
-todobody.classList.add("todo-body");
+  let todobody = document.createElement("div");
+  todobody.classList.add("todo-body");
 
-let todotext= document.createElement("h1");
-todotext.id="todo-title";
-todotext.innerText=input;
+  let todotext = document.createElement("h1");
+  todotext.id = "todo-title";
+  todotext.innerText = input;
 
-let tododone= document.createElement("div");
-tododone.classList.add("done");
+  let tododone = document.createElement("div");
+  tododone.classList.add("done");
 
-let tododelete= document.createElement("div");
-tododelete.classList.add("delete");
+  let tododelete = document.createElement("div");
+  tododelete.classList.add("delete");
 
-let tododoneimg= document.createElement("img");
-tododoneimg.src="./images/check-mark.svg";
+  let tododoneimg = document.createElement("img");
+  tododoneimg.src = "./images/check-mark.svg";
 
-let tododeleteimg= document.createElement("img");
-tododeleteimg.src="./images/trash.svg";
+  let tododeleteimg = document.createElement("img");
+  tododeleteimg.src = "./images/trash.svg";
 
-//append 
-todohead.appendChild(todotext);
-tododone.appendChild(tododoneimg);
-tododelete.appendChild(tododeleteimg);
-todobody.appendChild(tododone);
-todobody.appendChild(tododelete);
-todocard.appendChild(todohead);
-todocard.appendChild(todobody);
+  //append les different elements
+  todohead.appendChild(todotext);
+  tododone.appendChild(tododoneimg);
+  tododelete.appendChild(tododeleteimg);
+  todobody.appendChild(tododone);
+  todobody.appendChild(tododelete);
+  todocard.appendChild(todohead);
+  todocard.appendChild(todobody);
 
-let appendtodos= document.querySelector(".todos");
-appendtodos.appendChild(todocard);
-} 
+  //les rajouter sous la section ".todos"
+  let appendtodos = document.querySelector(".todos");
+  appendtodos.appendChild(todocard);
+}
